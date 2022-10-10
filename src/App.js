e71e407d79a040c4f309e65e492e76dff03d0b3c
arezki <zaidi_arezki@ymail.com>
2022-10-10 11:22:07 +0200

    feat: setup index page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg"
 import "./App.css"
+import HomePage from "./pages/HomePage"
 
 function App() {
-  return <div className="App">Bonjour</div>
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
 }
 
 export default App
