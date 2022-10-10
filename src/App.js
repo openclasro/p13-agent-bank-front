632e812524aab237dbcb0212b9e669cf62819bd7
arezki <zaidi_arezki@ymail.com>
2022-10-10 11:30:28 +0200

    refactor: nav and footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,26 @@ import HomePage from "./pages/HomePage"
 function App() {
   return (
     <div>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src={require("./assets/images/argentBankLogo.png")}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="./sign-in.html">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </a>
+        </div>
+      </nav>
       <HomePage />
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
     </div>
   )
 }
