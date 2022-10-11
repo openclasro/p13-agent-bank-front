bf6c3e3e57285b55591227e7acb81414cf74ff4a
arezki <zaidi_arezki@ymail.com>
2022-10-11 20:16:13 +0200

    supprimer non utilise

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import logo from "./logo.svg"
 import "./App.css"
-import HomePage from "./pages/HomePage"
-import SignInPage from "./pages/SignInPage"
-import UsersPage from "./pages/UsersPage"
 import { Outlet } from "react-router-dom"
 
-import { createBrowserRouter, Link } from "react-router-dom"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-])
+import { Link } from "react-router-dom"
 
 function App() {
   return (
