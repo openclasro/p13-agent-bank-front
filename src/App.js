b0885e2aa9633b55eccdbd8df45e183b08f7d6bb
arezki <zaidi_arezki@ymail.com>
2022-10-11 19:58:41 +0200

    feat: routes home, signin, users

--- src/App.js
+++ src/App.js
@@ -2,27 +2,39 @@ import logo from "./logo.svg"
 import "./App.css"
 import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/SignInPage"
+import UsersPage from "./pages/UsersPage"
+import { Outlet } from "react-router-dom"
+
+import { createBrowserRouter, Link } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+])
 
 function App() {
   return (
     <div>
       <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
+        <Link className="main-nav-logo" to="/">
           <img
             className="main-nav-logo-image"
             src={require("./assets/images/argentBankLogo.png")}
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
+
         <div>
-          <a className="main-nav-item" href="./sign-in.html">
+          <Link className="main-nav-item" to="/signin">
             <i className="fa fa-user-circle"></i>
             Sign In
-          </a>
+          </Link>
         </div>
       </nav>
-      <SignInPage />
+      <Outlet />
       <footer className="footer">
         <p className="footer-text">Copyright 2020 Argent Bank</p>
       </footer>
