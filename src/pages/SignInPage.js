33fdca273bdacde7087b5339fdf589eef9e6a95f
arezki <zaidi_arezki@ymail.com>
2022-10-16 15:02:35 +0200

    feat: signin connect

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,24 +1,63 @@
+import axios from "axios"
+import { useState } from "react"
+
 export default function SignInPage() {
+  const [username, setUsername] = useState("tony@stark.com")
+  const [password, setPassword] = useState("password123")
+
+  function signIn() {
+    axios
+      .post("http://localhost:3001/api/v1/user/login", {
+        email: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            signIn()
+          }}
+        >
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input
+              type="email"
+              id="username"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
 
-          <button className="sign-in-button">Sign In</button>
+          <button type="submit" className="sign-in-button">
+            Sign In
+          </button>
         </form>
       </section>
     </main>
